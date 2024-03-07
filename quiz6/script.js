9cf960bbe7fe9d49c69643c5bb5447de063c6d84
Anthony G <egiulia0@email.cpcc.edu>
2024-03-07 18:34:00 -0500

    Feat: implemented eventlistener for clicking on images

--- quiz6/script.js
+++ quiz6/script.js
@@ -1,6 +1,19 @@
 'use strict';
 
+const gallery = document.querySelector('.gallery');
 
+gallery.addEventListener('click', function (e) {
+    if (e.target.tagName === 'IMG') {
+        const prevImg = document.querySelector('.preview-container img.preview');
+
+        prevImg.src = e.target.src;
+        prevImg.alt = e.target.alt;
+
+        const figCap = document.querySelector('.preview-container figcaption');
+
+        figCap.textContent = e.target.alt;
+    }
+});
     
 
 
